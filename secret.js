68ca7f0bf618a9c41323cecd7b1e6087e68f8777
rahul74603 <rahulkumar74603@gmail.com>
2025-03-04 11:59:18 +0530

    Update secret.js

--- secret.js
+++ secret.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "your-project-id.firebaseapp.com",
-    databaseURL: "https://your-project-id-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "your-project-id",
-    storageBucket: "your-project-id.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDIDyD5MKcx3iQ2zHzpf_JPfUR3hxYlokQ",
+  authDomain: "jobxsuccess.firebaseapp.com",
+  databaseURL: "https://jobxsuccess-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "jobxsuccess",
+  storageBucket: "jobxsuccess.firebasestorage.app",
+  messagingSenderId: "300111417927",
+  appId: "1:300111417927:web:3f5240df7cb12f3941d762"
 };
