c37cf8204b8add090f6e027ee74cdb0c45bbf3b7
rahul74603 <rahulkumar74603@gmail.com>
2025-03-04 11:54:39 +0530

    Create secret.js

---
+++ secret.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "your-project-id.firebaseapp.com",
+    databaseURL: "https://your-project-id-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "your-project-id",
+    storageBucket: "your-project-id.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
