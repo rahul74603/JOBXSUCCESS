aac201a99c3cfa1e125bfe475373351c9b07e456
rahul74603 <rahulkumar74603@gmail.com>
2025-03-07 19:50:41 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
 const CACHE_NAME = 'jobxsuccess-v1';
-  const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    '/icon-192x192.png',
-    '/icon-512x512.png'
-  ];
+   const urlsToCache = [
+     '/',
+     '/index.html',
+     '/style.css',
+     '/script.js',
+     '/icon-192x192.png',
+     '/icon-512x512.png'
+   ];
 
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then((cache) => cache.addAll(urlsToCache))
-    );
-  });
+   self.addEventListener('install', (event) => {
+     event.waitUntil(
+       caches.open(CACHE_NAME)
+         .then((cache) => cache.addAll(urlsToCache))
+     );
+   });
 
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request)
-        .then((response) => response || fetch(event.request))
-    );
-  });
+   self.addEventListener('fetch', (event) => {
+     event.respondWith(
+       caches.match(event.request)
+         .then((response) => response || fetch(event.request))
+     );
+   });
