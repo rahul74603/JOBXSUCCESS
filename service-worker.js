b7fd03869626658b9a3568f91ba48af964eaaa2f
rahul74603 <rahulkumar74603@gmail.com>
2025-03-07 19:44:22 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,23 @@
-const CACHE_NAME = "jobxsuccess-cache-v3";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icons/icon-192x192.png",
-  "./icons/icon-512x512.png"
-];
+const CACHE_NAME = 'jobxsuccess-v1';
+  const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/icon-192x192.png',
+    '/icon-512x512.png'
+  ];
 
-// Install Service Worker & Cache Files
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    }).catch((error) => {
-      console.error("Cache addAll failed:", error);
-    })
-  );
-});
+  self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then((cache) => cache.addAll(urlsToCache))
+    );
+  });
 
-// Fetch Requests & Serve from Cache
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request).then((fetchResponse) => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, fetchResponse.clone());
-          return fetchResponse;
-        });
-      });
-    }).catch(() => {
-      return new Response("You are offline. Please check your internet connection.", {
-        status: 503,
-        statusText: "Service Unavailable"
-      });
-    })
-  );
-});
-
-// Activate & Update Service Worker
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => response || fetch(event.request))
+    );
+  });
