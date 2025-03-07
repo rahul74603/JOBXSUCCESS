42af882ae5c38306369817814d987ae111550f51
rahul74603 <rahulkumar74603@gmail.com>
2025-03-07 11:58:21 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,19 @@
 const CACHE_NAME = "jobxsuccess-cache-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+  self.location.origin + "/JOBXSUCCESS/",
+  self.location.origin + "/JOBXSUCCESS/index.html",
+  self.location.origin + "/JOBXSUCCESS/manifest.json",
+  self.location.origin + "/JOBXSUCCESS/icons/icon-192x192.png",
+  self.location.origin + "/JOBXSUCCESS/icons/icon-512x512.png"
 ];
 
 // Install Service Worker
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+      return cache.addAll(urlsToCache).catch((error) => {
+        console.error("Cache addAll failed: ", error);
+      });
     })
   );
 });
@@ -20,7 +22,9 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).catch(() => {
+        console.error("Fetch failed for ", event.request.url);
+      });
     })
   );
 });
