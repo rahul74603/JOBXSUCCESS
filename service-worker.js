3951abd3b75b4847a79c2e80622f05ec8688a7d0
rahul74603 <rahulkumar74603@gmail.com>
2025-03-07 20:05:29 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,29 @@
 const CACHE_NAME = 'jobxsuccess-v1';
-   const urlsToCache = [
-     '/',
-     '/index.html',
-     '/style.css',
-     '/script.js',
-     '/icon-192x192.png',
-     '/icon-512x512.png'
-   ];
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/jobs.html',
+  '/study.html'
+];
 
-   self.addEventListener('install', (event) => {
-     event.waitUntil(
-       caches.open(CACHE_NAME)
-         .then((cache) => cache.addAll(urlsToCache))
-     );
-   });
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
 
-   self.addEventListener('fetch', (event) => {
-     event.respondWith(
-       caches.match(event.request)
-         .then((response) => response || fetch(event.request))
-     );
-   });
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // अगर फाइल कैश में है, तो वही दिखाएं
+        if (response) {
+          return response;
+        }
+        // नहीं तो नेटवर्क से फाइल लोड करें
+        return fetch(event.request);
+      })
+  );
+});
