606b21bb76aecc35b7816fe7679d3cdf7deeb988
rahul74603 <rahulkumar74603@gmail.com>
2025-03-09 13:20:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 async function fetchJobData() {
     try {
-        let apiKey = "XXXX1"; // अपनी API Key डालो
+        let apiKey = "Thaviper@01"; // अपनी API Key डालो
         let apiUrl = "https://script.google.com/macros/s/AKfycbxnx750QfVkitn4Aoft2II7L-mw0DkzdN4I1OjW1vuEjODVt0uu8XvPlww80St5Txd37Q/exec?key=" + thaviper@01; 
 
         let response = await fetch(apiUrl);
