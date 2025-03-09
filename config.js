f1e428a009b6ba9ef93423b658ab2eec1c7630d2
rahul74603 <rahulkumar74603@gmail.com>
2025-03-09 11:44:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,17 @@
-// config.js
-
-async function loadConfig() {
-    let firebaseConfig = await getFirebaseConfig();
-    return {
-        SITE_NAME: "JOBXSUCCESS",
-        API_URL: firebaseConfig.API_URL, // 🔹 API URL को डायनामिक रूप से लोड करें
-    };
-}
-
-// 🔹 वेबसाइट का टाइटल अपडेट करना
-document.addEventListener("DOMContentLoaded", async function () {
-    let config = await loadConfig();
-    document.title = config.SITE_NAME;
-});
+fetch("https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec")
+  .then(response => response.json())
+  .then(data => {
+    let jobsContainer = document.getElementById("jobs");
+    jobsContainer.innerHTML = "";  
+    data.forEach(job => {
+      jobsContainer.innerHTML += `
+        <div>
+          <h3>${job["Job Title"]}</h3>
+          <p><b>Organization:</b> ${job.Organization}</p>
+          <p><b>Last Date:</b> ${job["Last Date"]}</p>
+          <a href="${job["Apply Link"]}" target="_blank">Apply Now</a>
+        </div>
+        <hr>
+      `;
+    });
+  });
