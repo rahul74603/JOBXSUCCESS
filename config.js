1ba2880972ecf2aedf8f56675c4d4591ff2976b7
rahul74603 <rahulkumar74603@gmail.com>
2025-03-10 08:35:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const config = {
-    API_URL: "https://script.google.com/macros/s/AKfycb.../exec" // ✅ Google Sheets API URL
+    API_URL: "https://script.google.com/macros/s/AKfycbxnx750QfVkitn4Aoft2II7L-mw0DkzdN4I1OjW1vuEjODVt0uu8XvPlww80St5Txd37Q/exec" // ✅ Google Sheets API URL
 };
 export default config;
