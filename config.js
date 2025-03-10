5ea7520e85b3594d5a5af7abd893e1e069578168
rahul74603 <rahulkumar74603@gmail.com>
2025-03-10 09:12:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-const config = {
-    API_URL: "https://script.google.com/macros/s/AKfycbxnx750QfVkitn4Aoft2II7L-mw0DkzdN4I1OjW1vuEjODVt0uu8XvPlww80St5Txd37Q/exec" // ✅ Google Sheets API URL
+window.config = {
+    API_URL: "https://script.google.com/macros/s/AKfycbxnx750QfVkitn4Aoft2II7L-mw0DkzdN4I1OjW1vuEjODVt0uu8XvPlww80St5Txd37Q/exec"
 };
-export default config;
