4a2944198fbe0be1e5c12f281af51dc256d3abbb
rahul74603 <rahulkumar74603@gmail.com>
2025-03-11 08:50:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,8 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxlmdCI1HHnQSwReOQ6Or-dNPdYoBWVBU3lRhas78H9etz5UgsYp_38ToWQZep0epY14w/exec"; 
-console.log("✅ API_URL Loaded:", API_URL);
+const apiKey = localStorage.getItem("apiKey"); 
+
+if (!apiKey) {
+    console.error("❌ API Key Not Found!");
+} else {
+    const API_URL = `https://script.google.com/macros/s/AKfycbxlmdCI1HHnQSwReOQ6Or-dNPdYoBWVBU3lRhas78H9etz5UgsYp_38ToWQZep0epY14w/exec?key=${apiKey}`;
+    console.log("✅ API_URL Loaded:", API_URL);
+}
