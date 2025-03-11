5e3f0301b56612fff499a98019da76674004de14
rahul74603 <rahulkumar74603@gmail.com>
2025-03-11 18:44:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const apiKey = localStorage.getItem("apiKey");
 if (!apiKey) {
     console.error("❌ API Key Not Found!");
 } else {
-    window.API_URL = `https://script.google.com/macros/s/AKfycbwJ_k5I3-gdgEme8JGTYG6EHTxoovaG44bJ6fU-qtvOaPqOipM8sc1IAREE04_XQi7P/exec?key=${apiKey}`;
+    window.API_URL = `https://script.google.com/macros/s/AKfycbyQqKOScQRE9Zt0l0tzplgnYoNnhrBT1HxfQhx-qqZdnVfZOdoa88hBsHh4vd0BzPUI/exec?key=${apiKey}`;
     console.log("✅ API_URL Loaded:", window.API_URL);
 }
