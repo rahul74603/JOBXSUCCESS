acc2af61d27f89c7479581d05d96a5956fc417f7
rahul74603 <rahulkumar74603@gmail.com>
2025-03-11 10:48:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const apiKey = localStorage.getItem("apiKey");
 if (!apiKey) {
     console.error("❌ API Key Not Found!");
 } else {
-    window.API_URL = `https://script.google.com/macros/s/AKfycbxIqMOWztisR_Yc_Vgtqt0nfpqGpH3Nl0IhDyUlh5CQ-DbTwsaam4Bpj5OXZAbv7JgU1w/exec?key=${apiKey}`;
+    window.API_URL = `https://script.google.com/macros/s/AKfycbwJ_k5I3-gdgEme8JGTYG6EHTxoovaG44bJ6fU-qtvOaPqOipM8sc1IAREE04_XQi7P/exec?key=${apiKey}`;
     console.log("✅ API_URL Loaded:", window.API_URL);
 }
