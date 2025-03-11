f2c3fb1bb698a3447e2c396c4e7fcb2766cf0464
rahul74603 <rahulkumar74603@gmail.com>
2025-03-11 09:04:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const apiKey = localStorage.getItem("apiKey"); 
+const apiKey = localStorage.getItem("apiKey");
 
 if (!apiKey) {
     console.error("❌ API Key Not Found!");
 } else {
-    const API_URL = `https://script.google.com/macros/s/AKfycbxlmdCI1HHnQSwReOQ6Or-dNPdYoBWVBU3lRhas78H9etz5UgsYp_38ToWQZep0epY14w/exec?key=${apiKey}`;
-    console.log("✅ API_URL Loaded:", API_URL);
+    window.API_URL = `https://script.google.com/macros/s/AKfycbxlmdCI1HHnQSwReOQ6Or-dNPdYoBWVBU3lRhas78H9etz5UgsYp_38ToWQZep0epY14w/exec?key=${apiKey}`;
+    console.log("✅ API_URL Loaded:", window.API_URL);
 }
